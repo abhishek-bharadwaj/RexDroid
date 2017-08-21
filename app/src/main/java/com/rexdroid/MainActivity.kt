df529b93823165b5d28e4bd4a4cbfdec07b842fd
Abhishek Bharadwaj <abhishek.bharadwaj@healthify.co>
2017-08-21 13:36:14 +0530

    Added onClicks

--- app/src/main/java/com/rexdroid/MainActivity.kt
+++ app/src/main/java/com/rexdroid/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.rexdroid
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.view.View
 import io.reactivex.Maybe
 import io.reactivex.MaybeObserver
 import io.reactivex.Single
@@ -10,26 +9,19 @@ import io.reactivex.SingleObserver
 import io.reactivex.disposables.Disposable
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     val TAG = javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        singlesExample()
-        mayBeExample()
-    }
-
-    private fun log(any: Any) {
-        Log.w(TAG, any.toString())
     }
 
     /**
      * No onComplete here as its meant to return one result
      */
-    private fun singlesExample() {
+    public fun singleExample(v: View) {
         val methodName = object : Any() {}.javaClass.enclosingMethod.name
         Single.just(Random().nextBoolean())
                 .subscribe(object : SingleObserver<Boolean> {
@@ -38,11 +30,11 @@ class MainActivity : AppCompatActivity() {
                     }
 
                     override fun onError(e: Throwable) {
-                        log("$methodName --> onError $e")
+                        log(TAG, "$methodName --> onError $e")
                     }
 
                     override fun onSuccess(t: Boolean) {
-                        log("$methodName --> onSuccess $t")
+                        log(TAG, "$methodName --> onSuccess $t")
                     }
                 })
 
@@ -51,17 +43,17 @@ class MainActivity : AppCompatActivity() {
     /**
      * It will be calling onSuccess or onComplete or onError
      */
-    private fun mayBeExample() {
+    public fun mayBeExample(v: View) {
         val methodName = object : Any() {}.javaClass.enclosingMethod.name
         Maybe.just(Random().nextBoolean())
                 .filter { it }
                 .subscribe(object : MaybeObserver<Boolean> {
                     override fun onSuccess(t: Boolean) {
-                        log("$methodName --> onSuccess $t")
+                        log(TAG, "$methodName --> onSuccess $t")
                     }
 
                     override fun onError(e: Throwable) {
-                        log("$methodName --> onError $e")
+                        log(TAG, "$methodName --> onError $e")
                     }
 
                     override fun onSubscribe(d: Disposable) {
@@ -69,7 +61,7 @@ class MainActivity : AppCompatActivity() {
                     }
 
                     override fun onComplete() {
-                        log("$methodName --> onComplete")
+                        log(TAG, "$methodName --> onComplete")
                     }
 
                 })
